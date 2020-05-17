c8aa35a0932eff36eaa7f14e9fa2e3f405bb7b4e
Keyur Vadodariya <vadodariyakeyur321@gmail.com>
2020-05-17 16:51:51 +0530

    Locally tested

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def load_data(nrows):
     #data = data[['date/time', 'latitude', 'longitude']]
     return data
 
-data = load_data(15000)
+data = load_data(100000)
 data[['latitude','longitude']].to_csv('lat_long.csv', index=False)
 
 
